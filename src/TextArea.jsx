3054739ec09cc8f28fbe5963882e94baf90fa1d9
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-11-03 18:14:06 +0600

    add font-color change

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -5,6 +5,8 @@ import logo from "./img/logo_aiu.png";
 const TextArea = () => {
   const [word, setWord] = useState("");
   const [bgColor, setBgColor] = useState(getRandomColor());
+  const [textColor, setTextColor] = useState("black");
+  const [inpColor, setInpColor] = useState("");
 
   function getRandomColor() {
     const letters = "0123456789ABCDEF";
@@ -21,15 +23,52 @@ const TextArea = () => {
     setBgColor(getRandomColor());
   };
 
+  const handleColorChange = (e) => {
+    setInpColor(e.target.value);
+  };
+  const applyColorChange = () => {
+    setTextColor(inpColor);
+  };
+
   return (
-    <div className="container" style={{ backgroundColor: bgColor }}>
-      <img className="logo" src={logo} alt="error" />
-      <input
-        type="text"
-        value={word}
-        onChange={handleInpChange}
-        placeholder="changes to a random bg color "
-      />
+    <div
+      className="container"
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      <div className="wrapper">
+        <div className="logo__block">
+          <h2 className="header">Alatoo International University</h2>
+          <img className="logo" src={logo} alt="error" />
+        </div>
+        <div className="main-block">
+          {" "}
+          <div className="bg-change__block">
+            <input
+              type="text"
+              value={word}
+              onChange={handleInpChange}
+              placeholder="changes to a random bg color "
+              className="inp-bg"
+            />
+          </div>
+          <div className="font-change__block">
+            <div className="font-change__block__main">
+              <input
+                className="inp-fc"
+                type="text"
+                placeholder="Enter color"
+                value={inpColor}
+                onChange={handleColorChange}
+              />
+              <button className="font-change__btn" onClick={applyColorChange}>
+                Apply
+              </button>
+            </div>
+
+            <p>changes font-color</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
